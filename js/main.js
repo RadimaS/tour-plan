11e1779633e668ea153f091b5fd91aef6f981aeb
RadimaS <selimova248@gmail.com>
2021-12-10 02:24:58 +0300

    create: settings validation and some  changes

--- js/main.js
+++ js/main.js
@@ -66,4 +66,23 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  //обработка формы
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Enter your name",
+          minlength: "The name must not be shorter than 2 characters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Enter your phone",
+        },
+      },
+    });
+  })
 });
