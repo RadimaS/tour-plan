e0be988a537320b7b4ba0681312e69fec8b0c895
RadimaS <selimova248@gmail.com>
2021-12-10 00:09:57 +0300

    create: create new repository and added all files

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+  const swiper = new Swiper(".hotel-slider", {
+    // Optional parameters
+
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+  });
+  const reviewsSlader = new Swiper(".reviews-slider", {
+    // Optional parameters
+
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  // var hotelSlider = new Swiper(".hotel-slider", {
+  //   loop: true,
+
+  //   navigation: {
+  //     nextEl: ".hotel-slider__button--next",
+  //     prevEl: ".hotel-slider__button--prev",
+  //   },
+  //   effect: "coverflow",
+  // });
+  // var reviewSlider = new Swiper(".reviews-slider", {
+  //   loop: true,
+
+  //   navigation: {
+  //     nextEl: ".reviews-slider__button--next",
+  //     prevEl: ".reviews-slider__button--prev",
+  //   },
+  // });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+});
